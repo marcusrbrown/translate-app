da3f118feeb3e1795c133b5f211ea39f50a43faa
Marcus R. Brown <contact@marcusrbrown.com>
2022-10-23 22:36:17 -0700

    feat: install Husky for Yarn >= 2 (#11)
    * feat: install Husky for Yarn >= 2
    
    * chore: format `src/` with the Prettier command used for `lint-staged`

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ root.render(
     <div className="wrapper">
       <App />
     </div>
-  </StrictMode>,
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
